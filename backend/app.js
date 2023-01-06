d037e9bc7b4dcf5bd09c2842858abb4a66a1bacf
LuuDucKhanh2022 <luuduckhanharsenal@.com>
2023-01-06 20:56:03 +0700

    text

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,13 @@ const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
